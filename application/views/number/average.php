56c8e52333af0acb2a93cd9fdc71b38b2ec6491f
Emi Berea <emi.berea@gmail.com>
2015-03-24 23:15:04 +0200

    updates

--- application/views/number/average.php
+++ application/views/number/average.php
@@ -3,6 +3,6 @@
     <div class="page-header">
         <h2>Average</h2>
     </div>
-    <h4>The numbers are: <?php echo implode(', ', $number_array); ?></h4>
-    <h3>The average is: <b><?php echo $average; ?></b></h3>
+    <h4>The numbers are: <?php if (isset($number_array)) { echo implode(', ', $number_array); } else { echo '-'; } ?></h4>
+    <h3>The average is: <b><?php if (isset($average)) { echo $average; } else { echo '-'; } ?></b></h3>
 </div>
